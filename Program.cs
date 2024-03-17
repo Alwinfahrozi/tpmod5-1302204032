f2ba4e0e70c1a1ce1226f44c86d2b7104b505e89
Alwinfahrozi <96618709+Alwinfahrozi@users.noreply.github.com>
2024-03-18 02:33:08 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*using System;
+
+public class HaloGeneric
+{
+    // B. Method SapaUser dengan generic parameter T
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo  " + user);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+
+        // C. Panggil method SapaUser dengan input nama panggilan praktikan (String)
+        string namaPanggilan = "Alwin";
+        halo.SapaUser(namaPanggilan);
+
+        // Contoh penggunaan dengan tipe data yang berbeda
+        int nomorPengguna = 1302204032;
+        halo.SapaUser(nomorPengguna);
+    }
+}*/
+
+using System;
+
+public class DataGeneric<T>
+{
+    // A. Property Data yang bertipe generic T
+    public T Data { get; set; }
+
+    // A. Konstruktor dengan parameter data
+    public DataGeneric(T data)
+    {
+        this.Data = data;
+    }
+
+    // B. Method PrintData untuk mencetak nilai data ke console
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + Data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // C. Panggil method PrintData() setelah mengisi "data" dengan NIM
+        DataGeneric<string> data = new DataGeneric<string>("1302204032");
+        data.PrintData();
+    }
+}

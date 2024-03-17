d96c2f70cc259fc8cbc0d6dabe5e01ec5c883944
Alwinfahrozi <96618709+Alwinfahrozi@users.noreply.github.com>
2024-03-18 02:30:05 +0700

    menambahkan class HeloGeneric dengan method SapaUser

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class HaloGeneric
+{
+    // B. Method SapaUser dengan generic parameter T
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo  " + user);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+
+        // C. Panggil method SapaUser dengan input nama panggilan praktikan (String)
+        string namaPanggilan = "Alwin";
+        halo.SapaUser(namaPanggilan);
+
+        // Contoh penggunaan dengan tipe data yang berbeda
+        int nomorPengguna = 1302204032;
+        halo.SapaUser(nomorPengguna);
+    }
+}
